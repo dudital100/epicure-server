02b0af48082af713967f29fc8c03d123aad19baa
David Tal <davidt@moveo.co.il>
2022-07-07 10:25:14 +0300

    adding rate limiter

--- Routers/authRouter.ts
+++ Routers/authRouter.ts
@@ -1,8 +1,17 @@
 const authRouter = require("express").Router();
 import authController from '../Controllers/authCntroller';
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    statusCode: 401, 
+    message: "Limit"
+})
 
 authRouter.post("/register", authController.register);
-authRouter.post("/login", authController.login);
-
+authRouter.post("/login",limiter, authController.login);
 
 export default authRouter;
